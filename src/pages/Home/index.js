2aa601a3d51ffbf2589740cb3c1737d946541bc3
Eduardo Faria <z.eduardofaria@gmail.com>
2020-03-09 15:45:46 -0300

    Styling home and cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      {Array.from(Array(9).keys()).map(item => (
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe1.jpg?ts=1571078789?resize=280:280"
+            alt="Tênis"
+          />
+          <strong>Tênis muito legal</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
