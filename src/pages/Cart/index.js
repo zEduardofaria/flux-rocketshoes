e95ed1003efdaf53aa90bb92f67403e5cdc8c6cc
Eduardo Faria <z.eduardofaria@gmail.com>
2020-03-10 16:14:16 -0300

    Adding amount in same id product

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -38,7 +38,7 @@ const Cart = () => {
                   <button type="button">
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
-                  <input type="number" readOnly value={1} />
+                  <input type="number" readOnly value={product.amount} />
                   <button type="button">
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
