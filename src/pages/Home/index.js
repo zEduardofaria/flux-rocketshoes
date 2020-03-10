747118e77060b064aac2985a658bf91796640352
Eduardo Faria <z.eduardofaria@gmail.com>
2020-03-10 15:42:56 -0300

    Removing useEffect error

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,15 +11,19 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
 
-  useEffect(async () => {
-    const response = await api.get('products');
+  useEffect(() => {
+    async function fetchData() {
+      const response = await api.get('products');
 
-    const data = response.data.map(product => ({
-      ...product,
-      priceFormatted: formatPrice(product.price),
-    }));
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
 
-    return setProducts(data);
+      setProducts(data);
+    }
+
+    fetchData();
   }, []);
 
   const HandleAddProduct = product => {
@@ -38,6 +42,24 @@ const Home = () => {
           <span>{product.priceFormatted}</span>
 
           <button type="button" onClick={() => HandleAddProduct(product)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+      {Array.from(Array(9).keys()).map(item => (
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe1.jpg?ts=1571078789?resize=280:280"
+            alt="Tênis"
+          />
+          <strong>Tênis muito legal</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
             <div>
               <MdAddShoppingCart size={16} color="#FFF" /> 3
             </div>
